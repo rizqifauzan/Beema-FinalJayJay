88c2a33ff04143b3dd8156a2acf71544e5675211
Rizqi Ahmad Fauzan <sipena.id@gmail.com>
2025-03-02 08:53:40 +0700

    add sleep 1s

--- src/test/java/stepDef/WebStep.java
+++ src/test/java/stepDef/WebStep.java
@@ -79,6 +79,7 @@ public class WebStep {
 
     @And("click detail item {string}")
     public void clickDetailItem(String item) throws InterruptedException {
-            productPage.clickItem(item);
+        Thread.sleep(1000);
+        productPage.clickItem(item);
     }
 }
